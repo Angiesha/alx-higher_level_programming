af4b5f46ce38a5f49e2c601a5d0621c16cc68ec2
Angiesha <angiesharon25477@gmail.com>
2022-07-15 11:39:47 +0300

    pyupdate

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    print("{}".format(chr(i)), end="")
+for letter in range(97, 122):
+    if chr(letter):
+        print("{}".format(chr(letter)), end="")
