aa2b868051b5ea8679397a1882d9d416c3647525
Angiesha <angiesharon25477@gmail.com>
2022-08-09 00:20:20 +0300

    classes&objects

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,71 +1,70 @@
 #!/usr/bin/python3
 
+"""Define a class Square."""
+
 
 class Square:
-    ''' class representing a square
-    '''
+    """Represent a square."""
+
     def __init__(self, size=0, position=(0, 0)):
-        ''' method initializing a square of size size
-        '''
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
+        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        ''' method that returns the size of the square
-        '''
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        ''' method that sets the size of the square
-        '''
-        if isinstance(value, int) is False:
-[O            raise TypeError('size must be an integer')
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = value
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     @property
     def position(self):
-        ''' method that returns the position of the square
-        '''
-        return self.__position
+        """Get/set the current position of the square."""
+        return (self.__position)
 
     @position.setter
     def position(self, value):
-        ''' method that sets the position of the square
-        '''
-        if isinstance(value, tuple) is False or len(value) != 2 or \
-           type(value[0]) != int or type(value[1]) != int or \
-           value[0] < 0 or value[1] < 0:
-            raise TypeError('position must be a tuple of 2 positive integers')
-        else:
-            self.__position = value
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
-        ''' method that returns area of the square
-        '''
-        return self.__size * self.__size
-
-    def __str__(self):
-        ''' printable representation of square
-        '''
-        toprint = ''
-        if self.__size == 0:
-            return toprint
-        for a in range((self.__position)[1]):
-            toprint = toprint + '\n'
-        for i in range(self.__size):
-            for b in range((self.__position)[0]):
-                toprint = toprint + ' '
-            for j in range(self.size):
-                toprint = toprint + '#'
-            toprint = toprint + '\n'
-        return toprint[:-1]
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
 
     def my_print(self):
-        ''' method that prints the square
-        '''
-        print(self)
+        """Print the square with the # character."""
+        if self.__size == 0:
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
+
+    def __str__(self):
+        """Define the print() representation of a Square."""
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")
