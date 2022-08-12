e434a1ce997fc318ac0c39e016b40b493ffca574
Angiesha <101760473+Angiesha@users.noreply.github.com>
2022-08-12 19:11:05 +0100

    Uploaded files

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -2,9 +2,11 @@
 
 def add(a, b):
     """My addition function
+
     Args:
         a: first integer
         b: second integer
+
     Returns:
         The return value. a + b
     """
@@ -13,9 +15,11 @@ def add(a, b):
 
 def sub(a, b):
     """My subtraction function
+
     Args:
         a: first integer
         b: second integer
+
     Returns:
         The return value. a - b
     """
@@ -24,9 +28,11 @@ def sub(a, b):
 
 def mul(a, b):
     """My multiplication function
+
     Args:
         a: first integer
         b: second integer
+
     Returns:
         The return value. a * b
     """
@@ -35,9 +41,11 @@ def mul(a, b):
 
 def div(a, b):
     """My division function
+
     Args:
         a: first integer
         b: second integer
+
     Returns:
         The return value. a / b
     """
