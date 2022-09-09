69fe7d46bb43123f561969c6b2b9204c823d4b04
Angiesha <angiesharon25477@gmail.com>
2022-09-09 23:13:57 +0300

    SQL queries

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,67 +1,17 @@
-===========================
-How to Use 3-say_my_name.py
-===========================
+#!/usr/bin/python3
+"""Defines a name-printing function."""
 
-This modules defines a function ``say_my_name(first_name, last_name="")``.
 
-Usage
-=====
-
-``say_my_name(...)`` prints "My name is <first_name> <last_name>".
-
-::
-
-    >>> say_my_name = __import__('3-say_my_name').say_my_name
-    >>> say_my_name("Angela", "Crabs")
-    My name is Angela Crabs
-
-::
-
-    >>> say_my_name("Bill Gates", "Bennie")
-    My name is Bill Gates Bennie
-
-The parameter ```last_name``` is optional. If no last name is provided,
-an empty string is printed instead.
-
-::
-
-    >>> say_my_name("Angela")
-    My name is Angela
-
-Invalid Names
-=============
-
-The parameters ``first_name`` and ``last_name``` must be strings. Otherwise,
-a TypeError is raised.
-
-::
-
-    >>> say_my_name(6, "Ronaldo")
-    Traceback (most recent call last):
-    TypeError: first_name must be a string
-
-::
-
-    >>> say_my_name("Cristiano", ["Jimin", "Jungkook", "Angie"])
-    Traceback (most recent call last):
-    TypeError: last_name must be a string
-
-::
-
-    >>> say_my_name({"Cristiano": 6, "Ronaldo": 37}, 6.1)
-    Traceback (most recent call last):
-    TypeError: first_name must be a string
-
-::
-
-    >>> say_my_name(None)
-    Traceback (most recent call last):
-    TypeError: first_name must be a string
-
-At least one name must be provided.
-
-::
-
-    >>> say_my_name()
-    Traceback (most recent call last):
-    TypeError: say_my_name() missing 1 required positional argument: 'first_name'
+def say_my_name(first_name, last_name=""):
+    """Print a name.
+    Args:
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
+    Raises:
+        TypeError: If either of first_name or last_name are not strings.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
